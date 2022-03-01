6a94fda3d68ffda2d6e58b86364e975dde6f6c5f
sazzadwasy <sazzad.wasy@gmail.com>
2022-03-01 20:17:51 +0600

    detail part done

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,7 @@
 const allMobiles = () => {
     const searchField = document.getElementById('search-field').value
+    console.log(searchField)
+
     // console.log(searchField)
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchField}`
     fetch(url)
@@ -11,19 +13,46 @@ const showMobileInUi = (mobiles) => {
     const mobileParent = document.getElementById('mobile-container')
     mobileParent.textContent = '';
     for (const mobile of mobiles) {
-        console.log(mobile)
+        // console.log(mobile)
         const div = document.createElement('div')
-        div.innerHTML = `<div class="col">
+        div.innerHTML = `<div class="col shadow">
         <div class="card h-100">
             <img src="${mobile.image}" class="w-50 m-auto" alt="...">
             <div class="card-body">
                 <h4>Name: ${mobile.phone_name}</h4>
                 <h4>Brand: ${mobile.brand}</h4>
-                <button id="explore-btn" class="btn btn-outline-secondary ">Explore</button>
+                <button onClick="getDetails('${mobile.slug}')" class="btn btn-outline-secondary ">Explore</button>
             </div>
         </div>
     </div>`
         mobileParent.appendChild(div)
     }
+
+}
+const getDetails = (mobileId) => {
+    console.log(mobileId)
+    const url = `https://openapi.programming-hero.com/api/phone/${mobileId}`
+    // console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showDetailOnTop(data.data))
+}
+const showDetailOnTop = (details) => {
+    console.log(details)
+    const topParent = document.getElementById('detail-container')
+    const div = document.createElement('div')
+    div.innerHTML = `<div class="card mb-3 shadow">
+    <img src="${details.image}" class="card-img-top w-25 m-auto" alt="...">
+    <div class="card-body">
+       <P class="fw-bold">Brand:${details.name}</P>
+       <P class="fw-bold">Brand:${details.brand}</P>
+       <P class="fw-bold">Storage: ${details.mainFeatures.storage}</P>
+       <P class="fw-bold">Display: ${details.mainFeatures.displaySize}</P>
+       <P class="fw-bold">Chipset: ${details.mainFeatures.chipSet}</P>
+       <P class="fw-bold">Usb: ${details.others.USB}</P>
+      
+    </div>
+</div>`
+    topParent.appendChild(div)
 
 }
