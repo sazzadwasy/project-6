75a62c54770bd3ea805ba664ff16e0fc106f9a9e
sazzadwasy <sazzad.wasy@gmail.com>
2022-03-01 17:23:30 +0600

    mobile show in ui

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,29 @@
-alert()
+const allMobiles = () => {
+    const searchField = document.getElementById('search-field').value
+    // console.log(searchField)
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchField}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showMobileInUi(data.data))
+}
+const showMobileInUi = (mobiles) => {
+    // console.log(mobiles)
+    const mobileParent = document.getElementById('mobile-container')
+    mobileParent.textContent = '';
+    for (const mobile of mobiles) {
+        console.log(mobile)
+        const div = document.createElement('div')
+        div.innerHTML = `<div class="col">
+        <div class="card h-100">
+            <img src="${mobile.image}" class="w-50 m-auto" alt="...">
+            <div class="card-body">
+                <h4>Name: ${mobile.phone_name}</h4>
+                <h4>Brand: ${mobile.brand}</h4>
+                <button id="explore-btn" class="btn btn-outline-secondary ">Explore</button>
+            </div>
+        </div>
+    </div>`
+        mobileParent.appendChild(div)
+    }
+
+}
